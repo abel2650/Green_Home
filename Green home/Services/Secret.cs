eca00c487e5911e050c70cfae5c283bd95224b11
rebin <rei001@edu.zealand.dk>
2024-05-13 10:01:44 +0200

    .

---
+++ Green home/Services/Secret.cs
@@ -0,0 +1,11 @@
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Green_home.Services
+{
+    public class Secret
+    {
+        public static string ConnectionString => "Data Source=mssql14.unoeuro.com;Initial Catalog = mnhart_dk_db_datawebshop; User ID = mnhart_dk; Password=htBz5nac3Ek94x6g2bRd;Connect Timeout = 30; Encrypt=False;Trust Server Certificate=False;Application Intent = ReadWrite; Multi Subnet Failover=False";
+    }
+}
