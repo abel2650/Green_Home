6317ae92f78e4b09b998033a6832df6e2e6afd00
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2024-05-13 09:49:59 +0200

    Om os page og Nav bar 1

---
+++ Green home/Pages/Om_os.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_home.Pages
+{
+    public class Om_osModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
