60064cf39d9033273d9816e05b6f1bbe6a42c20a
rebin <rei001@edu.zealand.dk>
2024-05-14 14:00:43 +0200

    Created redirecttopage

---
+++ Green home/Pages/CreateEjendomme.cshtml.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel.DataAnnotations;
+using Green_home.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Green_home.Model;
+
+namespace Green_home.Pages
+{
+    public class CreateEjendomme : PageModel
+    {
+        private readonly EjendommeRepository_DB _ejendommeRepository;
+
+        [BindProperty]
+        [Required(ErrorMessage = "Feltet er påkrævet"), MaxLength(50)]
+        public int Id { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Feltet er påkrævet")]
+        public float Pris { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Feltet er påkrævet"), MaxLength(1)]
+        public string Energimærke { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Feltet er påkrævet")]
+        public int Kvm { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Feltet er påkrævet")]
+        public int By_id { get; set; }
+
+        public CreateEjendomme(EjendommeRepository_DB ejendommeRepository)
+        {
+            _ejendommeRepository = ejendommeRepository;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            Green_home.Model.Ejendomme ejendomme = new Green_home.Model.Ejendomme(Id, Pris, Kvm, Energimærke, By_id);
+            _ejendommeRepository.AddEjendomme(ejendomme);
+
+            return RedirectToPage("/EjendeommeModel");
+        }
+    }
+}
