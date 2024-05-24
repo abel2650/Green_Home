b7b1b8a1f4525b80b18d3b50f8d600e8a640382a
Marcus Nørregaard Hartmann <mah016@edu.zealand.dk>
2024-05-24 14:19:53 +0200

    Kontakt side

---
+++ Green home/Pages/KontaktOs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Green_home.Pages
+{
+    public class KontaktOsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
